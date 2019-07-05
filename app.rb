10046126efa942fdad756bb378aa7f59f21d8328
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-07-05 15:29:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,10 +28,9 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do 
-     @operation = params[:operation]
+    @operation = params[:operation]
     @number1 = params [:number1].to_i
     @number2 = params[:number2].to_i
-   
     erb :operation
   end
 end
